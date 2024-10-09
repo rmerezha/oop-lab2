289ddb21e556b7a7f20e5b7730648477117b3481
Merezha Roman <rmerezha@gmail.com>
2024-10-09 22:39:20 +0300

    Change bug

--- src/main/java/rmerezha/editor/EllipseEditor.java
+++ src/main/java/rmerezha/editor/EllipseEditor.java
@@ -2,6 +2,7 @@ package rmerezha.editor;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import rmerezha.shape.EllipseShape;
 import rmerezha.shape.LineShape;
 import rmerezha.shape.Shape;
@@ -17,4 +18,18 @@ public class EllipseEditor extends ShapeEditor{
         shape = new EllipseShape();
     }
 
+    @Override
+    public void onMouseMove(MouseEvent event) {
+        onPaint();
+        Point currentPoint = new Point(event.getX(), event.getY());
+
+        double minX2 = Point.minX(shape.getP1(), currentPoint);
+        double minY2 = Point.minY(shape.getP1(), currentPoint);
+        double w2 = Point.w(shape.getP1(), currentPoint);
+        double h2 = Point.h(shape.getP1(), currentPoint);
+
+        gc.setStroke(Color.RED);
+        gc.strokeOval(minX2, minY2, w2, h2);
+        gc.setStroke(Color.BLACK);
+    }
 }
--- src/main/java/rmerezha/editor/LineEditor.java
+++ src/main/java/rmerezha/editor/LineEditor.java
@@ -2,6 +2,7 @@ package rmerezha.editor;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
 import rmerezha.shape.LineShape;
 import rmerezha.shape.PointShape;
 import rmerezha.shape.Shape;
@@ -17,4 +18,18 @@ public class LineEditor extends ShapeEditor {
         shape = new LineShape();
     }
 
+    @Override
+    public void onMouseMove(MouseEvent event) {
+        onPaint();
+        Point currentPoint = new Point(event.getX(), event.getY());
+
+        double minX2 = Point.minX(shape.getP1(), currentPoint);
+        double minY2 = Point.minY(shape.getP1(), currentPoint);
+        double w2 = Point.w(shape.getP1(), currentPoint);
+        double h2 = Point.h(shape.getP1(), currentPoint);
+
+        gc.setStroke(Color.RED);
+        gc.strokeLine(shape.getP1().x(), shape.getP1().y(), currentPoint.x(), currentPoint.y());
+        gc.setStroke(Color.BLACK);
+    }
 }
